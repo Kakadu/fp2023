6e58151b60e6b2a5f53e1d53a140bf4daa509fe6
Arsene-Baitenov <arsenebaitenov@gmail.com>
2023-10-30 08:43:10 +0300

    test: Add expected tests of expr parser.

---
+++ Cypher/lib/test.ml
@@ -0,0 +1,76 @@
+(** Copyright 2023 Arseniy Baytenov *)
+
+(** SPDX-License-Identifier: MIT *)
+
+open Base
+open Cypher_lib.Ast
+open Cypher_lib.Parser
+
+let parse_and_print s = Stdlib.Format.printf "%a" pp_expression (parse_expr s)
+
+let%expect_test "Simple expr" =
+  parse_and_print {| 120.607 + 400 / 10 ^ 5 |};
+  [%expect
+    {|
+  (Bin_op (Plus, (Const (Float 120.607)),
+     (Bin_op (Slash, (Const (Int64 400L)),
+        (Bin_op (Caret, (Const (Int64 10L)), (Const (Int64 5L))))))
+     )) |}]
+;;
+
+let%expect_test "Simple expr with unary minus" =
+  parse_and_print {|- 120.607 + -400 / 10 ^ -x |};
+  [%expect
+    {|
+  (Bin_op (Plus, (Un_op (Minus, (Const (Float 120.607)))),
+     (Bin_op (Slash, (Const (Int64 -400L)),
+        (Bin_op (Caret, (Const (Int64 10L)), (Un_op (Minus, (Var "x")))))))
+     )) |}]
+;;
+
+let%expect_test "Multiple sequential comparison operators" =
+  parse_and_print {| 1+-1 = 0 <> 10 >= 5*1 <= 5/1 > - -4 |};
+  [%expect
+    {|
+  (List_op ((Bin_op (Plus, (Const (Int64 1L)), (Const (Int64 -1L)))),
+     [(Eq, (Const (Int64 0L))); (NEq, (Const (Int64 10L)));
+       (GEq, (Bin_op (Asterisk, (Const (Int64 5L)), (Const (Int64 1L)))));
+       (LEq, (Bin_op (Slash, (Const (Int64 5L)), (Const (Int64 1L)))));
+       (Greater, (Un_op (Minus, (Const (Int64 -4L)))))]
+     )) |}]
+;;
+
+let%expect_test "Null check operators" =
+  parse_and_print {| 4 = (4 + a is null IS NOT NULL) is null |};
+  [%expect.unreachable]
+[@@expect.uncaught_exn
+  {|
+  (* CR expect_test_collector: This test expectation appears to contain a backtrace.
+     This is strongly discouraged as backtraces are fragile.
+     Please change this test to not include a backtrace. *)
+
+  (Failure ": end_of_input")
+  Raised at Stdlib.failwith in file "stdlib.ml", line 29, characters 17-33
+  Called from Test.parse_and_print in file "lib/test.ml", line 9, characters 64-78
+  Called from Test.(fun) in file "lib/test.ml", line 44, characters 2-63
+  Called from Expect_test_collector.Make.Instance.exec in file "collector/expect_test_collector.ml", line 244, characters 12-19 |}]
+;;
+
+let%expect_test "Boolean oparators and literals" =
+  parse_and_print
+    {| not 1 = 1 or 4 = a + 23.0 and false xor not true or "Hello" = a + "llo" |};
+  [%expect
+    {|
+  (Bin_op (OR,
+     (Bin_op (OR,
+        (Un_op (NOT, (List_op ((Const (Int64 1L)), [(Eq, (Const (Int64 1L)))]))
+           )),
+        (Bin_op (AND,
+           (List_op ((Const (Int64 4L)),
+              [(Eq, (Bin_op (Plus, (Var "a"), (Const (Float 23.)))))])),
+           (Bin_op (XOR, (Liter False), (Un_op (NOT, (Liter True)))))))
+        )),
+     (List_op ((Const (String "Hello")),
+        [(Eq, (Bin_op (Plus, (Var "a"), (Const (String "llo")))))]))
+     )) |}]
+;;
