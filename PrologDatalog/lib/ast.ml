b100e3339b87873c4bd084952bc74d7d534e6fdf
twotwozeronine <st112283@mail.ru>
2023-11-07 01:48:47 +0300

    Fix: modern AST files

--- PrologDatalog/lib/ast.ml
+++ PrologDatalog/lib/ast.ml
@@ -2,27 +2,28 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-type name = string [@@deriving eq, show { with_path = false }]
-type num = int [@@deriving eq, show { with_path = false }]
-type oper = string [@@deriving eq, show { with_path = false }]
+type name = string [@@deriving show { with_path = false }]
+type num = int [@@deriving show { with_path = false }]
+type oper = string [@@deriving show { with_path = false }]
+type var = string [@@deriving show { with_path = false }]
 
 type atom =
   | Name of name
   | Oper of oper
-[@@deriving eq, show { with_path = false }]
+[@@deriving show { with_path = false }]
 
 type const =
   | Num of num
   | Atom of atom
-[@@deriving eq, show { with_path = false }]
+[@@deriving show { with_path = false }]
 
 type term =
   | Const of const
-  | Var of string
+  | Var of var
   | Relation of
       { atom : atom
       ; terms : term list
       }
-[@@deriving eq, show { with_path = false }]
+[@@deriving show { with_path = false }]
 
-type many_term = Many_term of term list [@@deriving eq, show { with_path = false }]
+type many_term = Many_term of term list [@@deriving show { with_path = false }]
--- PrologDatalog/lib/ast.mli
+++ PrologDatalog/lib/ast.mli
@@ -2,36 +2,52 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-type atom =
-  | Name of string
-  | Oper of string
+type name = string
 
-val equal_atom : atom -> atom -> bool
+val pp_name : Format.formatter -> name -> unit
+val show_name : name -> string
+
+type num = int
+
+val pp_num : Format.formatter -> num -> unit
+val show_num : num -> string
+
+type oper = string
+
+val pp_oper : Format.formatter -> oper -> unit
+val show_oper : oper -> string
+
+type var = string
+
+val pp_var : Format.formatter -> var -> unit
+val show_var : var -> string
+
+type atom =
+  | Name of name
+  | Oper of oper
+
 val pp_atom : Format.formatter -> atom -> unit
 val show_atom : atom -> string
 
 type const =
-  | Num of int
+  | Num of num
   | Atom of atom
 
-val equal_const : const -> const -> bool
 val pp_const : Format.formatter -> const -> unit
 val show_const : const -> string
 
 type term =
   | Const of const
-  | Var of string
+  | Var of var
   | Relation of
       { atom : atom
       ; terms : term list
       }
 
-val equal_term : term -> term -> bool
 val pp_term : Format.formatter -> term -> unit
 val show_term : term -> string
 
 type many_term = Many_term of term list
 
-val equal_many_term : many_term -> many_term -> bool
 val pp_many_term : Format.formatter -> many_term -> unit
 val show_many_term : many_term -> string
