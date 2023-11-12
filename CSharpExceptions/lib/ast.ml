a1b27adf9cab3fd6199080dc068551a25cc63696
Georgy S <it.is.mr.lag@gmail.com>
2023-11-12 19:55:50 +0300

    refactor(ast, dune): Remove ppx_deriving.eq

--- CSharpExceptions/lib/ast.ml
+++ CSharpExceptions/lib/ast.ml
@@ -10,9 +10,9 @@ type value_ =
   | VInt of int
   | VChar of char
   | VBool of bool
-[@@deriving show { with_path = false }, eq]
+[@@deriving show { with_path = false }]
 
-type ident = Id of string [@@deriving show { with_path = false }, eq]
+type ident = Id of string [@@deriving show { with_path = false }]
 
 (** {2 Declarations types} *)
 
@@ -24,48 +24,48 @@ type base_type =
   | TInt
   | TChar
   | TBool
-[@@deriving show { with_path = false }, eq]
+[@@deriving show { with_path = false }]
 
 type nulable_type =
   | TBase of base_type
   | TString
   | TClass of ident
-[@@deriving show { with_path = false }, eq]
+[@@deriving show { with_path = false }]
 
 type assignable_type =
   | TNot_Nullable of base_type
   | TNullable of nulable_type
-[@@deriving show { with_path = false }, eq]
+[@@deriving show { with_path = false }]
 
-type var_type = TVar of assignable_type [@@deriving show { with_path = false }, eq]
+type var_type = TVar of assignable_type [@@deriving show { with_path = false }]
 
 type meth_type =
   | Void
   | TReturn of assignable_type
-[@@deriving show { with_path = false }, eq]
+[@@deriving show { with_path = false }]
 
 type type_ =
   | TMethod of meth_type
   | TVariable of var_type
-[@@deriving show { with_path = false }, eq]
+[@@deriving show { with_path = false }]
 
 type access_modifier =
   | MPublic
   | MPrivate
   | MProtected
-[@@deriving show { with_path = false }, eq]
+[@@deriving show { with_path = false }]
 
 type method_modifier =
   | MAccess of access_modifier
   | MStatic
 (* TODO: | Virtual *)
 (* TODO: | Override *)
-[@@deriving show { with_path = false }, eq]
+[@@deriving show { with_path = false }]
 
 type fild_modifier = FAccess of access_modifier
 (* TODO:| New *)
 (* TODO:| Const *)
-[@@deriving show { with_path = false }, eq]
+[@@deriving show { with_path = false }]
 
 type bin_op =
   | Asterisk (* [*] *)
@@ -82,13 +82,13 @@ type bin_op =
   | And (* [&&] *)
   | Or (* [||] *)
   | Assign (* [=] *)
-[@@deriving show { with_path = false }, eq]
+[@@deriving show { with_path = false }]
 
 type un_op =
   | UMinus (* [-] *)
   | UNot (* [!] *)
   | New (* [new] *)
-[@@deriving show { with_path = false }, eq]
+[@@deriving show { with_path = false }]
 
 type expr =
   | EConst of value_ (* assignable values *)
@@ -102,10 +102,10 @@ type expr =
 (*  *)
 (* TODO: | Cast of assignable_type * expr *)
 
-and params = Params of expr list [@@deriving show { with_path = false }, eq]
+and params = Params of expr list [@@deriving show { with_path = false }]
 
-type var_decl = Var_decl of type_ * ident [@@deriving show { with_path = false }, eq]
-type args = Args of var_decl list [@@deriving show { with_path = false }, eq]
+type var_decl = Var_decl of type_ * ident [@@deriving show { with_path = false }]
+type args = Args of var_decl list [@@deriving show { with_path = false }]
 
 type statement =
   | SExpr of expr
@@ -114,7 +114,7 @@ type statement =
   | SDecl of var_decl * expr option
   | SReturn of expr option
   | SBreak
-[@@deriving show { with_path = false }, eq]
+[@@deriving show { with_path = false }]
 
 (* TODO:| SWhile *)
 (* TODO:| SFor *)
@@ -126,7 +126,7 @@ type fild_sign =
   ; f_id : ident
   ; f_val : expr option
   }
-[@@deriving show { with_path = false }, eq]
+[@@deriving show { with_path = false }]
 
 type method_sign =
   { m_modif : method_modifier option
@@ -134,7 +134,7 @@ type method_sign =
   ; m_id : ident
   ; m_args : args
   }
-[@@deriving show { with_path = false }, eq]
+[@@deriving show { with_path = false }]
 
 type constructor_sign =
   { con_modif : access_modifier option
@@ -142,14 +142,14 @@ type constructor_sign =
   ; con_args : args
   ; base_params : params option
   }
-[@@deriving show { with_path = false }, eq]
+[@@deriving show { with_path = false }]
 
 type class_member =
   | Fild of fild_sign
   | Main of statement
   | Method of method_sign * statement (* statment - Steps *)
   | Constructor of constructor_sign * statement (* statment - Steps *)
-[@@deriving show { with_path = false }, eq]
+[@@deriving show { with_path = false }]
 
 type class_decl =
   { cl_modif : access_modifier option
@@ -157,6 +157,6 @@ type class_decl =
   ; parent : ident option
   ; cl_mems : class_member list
   }
-[@@deriving show { with_path = false }, eq]
+[@@deriving show { with_path = false }]
 
-type tast = Ast of class_decl list [@@deriving show { with_path = false }, eq]
+type tast = Ast of class_decl list [@@deriving show { with_path = false }]
