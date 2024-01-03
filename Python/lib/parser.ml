2e085aad2f789a7e1b0153a55eb07dde6fa4ee51
pasha <pasha@kub.myguest.virtualbox.org>
2024-01-03 09:29:30 +0300

    fix tests

--- Python/lib/parser.ml
+++ Python/lib/parser.ml
@@ -562,233 +562,281 @@ let pyParser =
 
 let parser s = parse pyParser s
 
-let%test _ =
-  parser "print(\"Hello World\")"
-  = Ok
-      [ Expression (FunctionCall (Identifier "print", [ Const (String "Hello World") ])) ]
+let test_parse_res parser input =
+  Angstrom.parse_string ~consume:Consume.Prefix parser input
 ;;
 
-let%test _ = parse pyParser "1" = Ok [ Expression (Const (Int 1)) ]
-
-let%test _ =
-  parse pyParser "if y == 3:\n\tx = 0"
-  = Ok
-      [ IfElse
-          ( BoolOp (Equal, Variable (Global, Identifier "y"), Const (Int 3))
-          , [ Assign (Variable (Global, Identifier "x"), Const (Int 0)) ]
-          , [] )
-      ]
+let parser_tester parser show input =
+  match test_parse_res parser input with
+  | Result.Error _ -> Format.printf "Error"
+  | Result.Ok statements ->
+    List.iter (fun e -> print_endline (show_statement e)) statements
 ;;
 
-let%test _ =
-  parse pyParser "myFunction(x)"
-  = Ok
-      [ Expression
-          (FunctionCall (Identifier "myFunction", [ Variable (Global, Identifier "x") ]))
-      ]
+let%expect_test _ =
+  parser_tester pyParser show_statement {| print("Hello World") |};
+  [%expect
+    {|
+        (Expression
+           (FunctionCall ((Identifier "print"), [(Const (String "Hello World"))]))) |}]
 ;;
 
-let%test _ =
-  parse pyParser "def testFunction(x):\n    x = 1\n    return x + 1"
-  = Ok
-      [ Function
-          ( Identifier "testFunction"
-          , [ Identifier "x" ]
-          , [ Assign (Variable (Global, Identifier "x"), Const (Int 1))
-            ; Return (ArithOp (Add, Variable (Global, Identifier "x"), Const (Int 1)))
-            ] )
-      ]
+let%expect_test _ =
+  parser_tester pyParser show_statement "1";
+  [%expect {|(Expression (Const (Int 1))) |}]
 ;;
 
-let%test _ =
-  parse pyParser "while (y == 2):\n    x = 2"
-  = Ok
-      [ While
-          ( BoolOp (Equal, Variable (Global, Identifier "y"), Const (Int 2))
-          , [ Assign (Variable (Global, Identifier "x"), Const (Int 2)) ] )
-      ]
+let%expect_test _ =
+  parser_tester pyParser show_statement {|
+  if y == 3:
+      x = 0
+  |};
+  [%expect
+    {|
+      (IfElse (
+         (BoolOp (Equal, (Variable (Global, (Identifier "y"))), (Const (Int 3)))),
+         [(Assign ((Variable (Global, (Identifier "x"))), (Const (Int 0))))],
+         [])) |}]
 ;;
 
-let%test _ =
-  parse
+let%expect_test _ =
+  parser_tester pyParser show_statement "myFunction(x)";
+  [%expect
+    {|
+    (Expression
+       (FunctionCall ((Identifier "myFunction"),
+          [(Variable (Global, (Identifier "x")))]))) |}]
+;;
+
+let%expect_test _ =
+  parser_tester
     pyParser
-    "\n\
-     def factorial(x):\n\
-    \    if (x == 1):\n\
-    \        return 1\n\
-    \    else:\n\
-    \        return (x * factorial(x - 1))"
-  = Ok
-      [ Function
-          ( Identifier "factorial"
-          , [ Identifier "x" ]
-          , [ IfElse
-                ( BoolOp (Equal, Variable (Global, Identifier "x"), Const (Int 1))
-                , [ Return (Const (Int 1)) ]
-                , [ Return
-                      (ArithOp
-                         ( Mul
-                         , Variable (Global, Identifier "x")
-                         , FunctionCall
-                             ( Identifier "factorial"
-                             , [ ArithOp
-                                   (Sub, Variable (Global, Identifier "x"), Const (Int 1))
-                               ] ) ))
-                  ] )
-            ] )
-      ]
+    show_statement
+    {|
+    def testFunction(x):
+        x = 1
+        return x + 1 |};
+  [%expect
+    {|
+      (Function ((Identifier "testFunction"), [(Identifier "x")],
+         [(Assign ((Variable (Global, (Identifier "x"))), (Const (Int 1))));
+           (Return
+              (ArithOp (Add, (Variable (Global, (Identifier "x"))), (Const (Int 1))
+                 )))
+           ]
+         )) |}]
 ;;
 
-let%test _ =
-  parse pyParser "(y == 2)"
-  = Ok [ Expression (BoolOp (Equal, Variable (Global, Identifier "y"), Const (Int 2))) ]
+let%expect_test _ =
+  parser_tester pyParser show_statement {|
+  while (y == 2):
+      x = 2 |};
+  [%expect
+    {|
+      (While (
+         (BoolOp (Equal, (Variable (Global, (Identifier "y"))), (Const (Int 2)))),
+         [(Assign ((Variable (Global, (Identifier "x"))), (Const (Int 2))))])) |}]
 ;;
 
-let%test _ =
-  parse pyParser "(y >= 2)"
-  = Ok
-      [ Expression
-          (BoolOp (GreaterOrEqual, Variable (Global, Identifier "y"), Const (Int 2)))
-      ]
+let%expect_test _ =
+  parser_tester
+    pyParser
+    show_statement
+    {|
+    def factorial(x):
+        if (x == 1):
+            return 1
+        else:
+            return (x * factorial(x - 1)) |};
+  [%expect
+    {|
+      (Function ((Identifier "factorial"), [(Identifier "x")],
+         [(IfElse (
+             (BoolOp (Equal, (Variable (Global, (Identifier "x"))), (Const (Int 1))
+                )),
+             [(Return (Const (Int 1)))],
+             [(Return
+                 (ArithOp (Mul, (Variable (Global, (Identifier "x"))),
+                    (FunctionCall ((Identifier "factorial"),
+                       [(ArithOp (Sub, (Variable (Global, (Identifier "x"))),
+                           (Const (Int 1))))
+                         ]
+                       ))
+                    )))
+               ]
+             ))
+           ]
+         )) |}]
 ;;
 
-let%test _ =
-  parse pyParser "(y > 2)"
-  = Ok [ Expression (BoolOp (Greater, Variable (Global, Identifier "y"), Const (Int 2))) ]
+let%expect_test _ =
+  parser_tester pyParser show_statement "(y == 2)";
+  [%expect
+    {|
+    (Expression
+       (BoolOp (Equal, (Variable (Global, (Identifier "y"))), (Const (Int 2))))) |}]
 ;;
 
-let%test _ =
-  parse pyParser "(y < 2)"
-  = Ok [ Expression (BoolOp (Less, Variable (Global, Identifier "y"), Const (Int 2))) ]
+let%expect_test _ =
+  parser_tester pyParser show_statement "(y >= 2)";
+  [%expect
+    {|
+    (Expression
+       (BoolOp (GreaterOrEqual, (Variable (Global, (Identifier "y"))),
+          (Const (Int 2))))) |}]
 ;;
 
-let%test _ =
-  parse pyParser "(y <= 2)"
-  = Ok
-      [ Expression
-          (BoolOp (LessOrEqual, Variable (Global, Identifier "y"), Const (Int 2)))
-      ]
+let%expect_test _ =
+  parser_tester pyParser show_statement "(y > 2)";
+  [%expect
+    {|
+    (Expression
+       (BoolOp (Greater, (Variable (Global, (Identifier "y"))), (Const (Int 2))))) |}]
 ;;
 
-let%test _ =
-  parse pyParser "(y != 2)"
-  = Ok
-      [ Expression (BoolOp (NotEqual, Variable (Global, Identifier "y"), Const (Int 2))) ]
+let%expect_test _ =
+  parser_tester pyParser show_statement "(y < 2)";
+  [%expect
+    {|
+    (Expression
+       (BoolOp (Less, (Variable (Global, (Identifier "y"))), (Const (Int 2))))) |}]
 ;;
 
-let%test _ =
-  parse pyParser "(x and y)"
-  = Ok
-      [ Expression
-          (BoolOp
-             (And, Variable (Global, Identifier "x"), Variable (Global, Identifier "y")))
-      ]
+let%expect_test _ =
+  parser_tester pyParser show_statement "(y <= 2)";
+  [%expect
+    {|
+    (Expression
+       (BoolOp (LessOrEqual, (Variable (Global, (Identifier "y"))),
+          (Const (Int 2))))) |}]
 ;;
 
-let%test _ =
-  parse pyParser "(x or y)"
-  = Ok
-      [ Expression
-          (BoolOp
-             (Or, Variable (Global, Identifier "x"), Variable (Global, Identifier "y")))
-      ]
+let%expect_test _ =
+  parser_tester pyParser show_statement "(y != 2)";
+  [%expect
+    {|
+    (Expression
+       (BoolOp (NotEqual, (Variable (Global, (Identifier "y"))), (Const (Int 2))
+          ))) |}]
 ;;
 
-let%test _ =
-  parse pyParser "\nif (x > 0):\n    y = 2\n else:\n    y = 1"
-  = Ok
-      [ IfElse
-          ( BoolOp (Greater, Variable (Global, Identifier "x"), Const (Int 0))
-          , [ Assign (Variable (Global, Identifier "y"), Const (Int 2)) ]
-          , [ Assign (Variable (Global, Identifier "y"), Const (Int 1)) ] )
-      ]
+let%expect_test _ =
+  parser_tester pyParser show_statement "(x and y)";
+  [%expect
+    {|
+    (Expression
+       (BoolOp (And, (Variable (Global, (Identifier "x"))),
+          (Variable (Global, (Identifier "y")))))) |}]
+;;
+
+let%expect_test _ =
+  parser_tester pyParser show_statement "(x or y)";
+  [%expect
+    {|
+    (Expression
+       (BoolOp (Or, (Variable (Global, (Identifier "x"))),
+          (Variable (Global, (Identifier "y")))))) |}]
+;;
+
+let%expect_test _ =
+  parser_tester
+    pyParser
+    show_statement
+    {|
+  if (x > 0):
+      y = 2
+  else:
+      y = 1 |};
+  [%expect
+    {|
+    (IfElse (
+       (BoolOp (Greater, (Variable (Global, (Identifier "x"))), (Const (Int 0)))),
+       [(Assign ((Variable (Global, (Identifier "y"))), (Const (Int 2))))],
+       [(Assign ((Variable (Global, (Identifier "y"))), (Const (Int 1))))])) |}]
+;;
+
+let%expect_test _ =
+  parser_tester pyParser show_statement {|
+for i in range(0, 3):
+    print(i) |};
+  [%expect
+    {|
+    (For ((Variable (Global, (Identifier "i"))),
+       [(Const (Int 0)); (Const (Int 3))],
+       [(Expression
+           (FunctionCall ((Identifier "print"),
+              [(Variable (Global, (Identifier "i")))])))
+         ]
+       )) |}]
+;;
+
+let%expect_test _ =
+  parser_tester pyParser show_statement {|
+  while 1 == 1:
+      print(1) |};
+  [%expect
+    {|
+    (While ((BoolOp (Equal, (Const (Int 1)), (Const (Int 1)))),
+       [(Expression (FunctionCall ((Identifier "print"), [(Const (Int 1))])))])) |}]
+;;
+
+let%expect_test _ =
+  parser_tester
+    pyParser
+    show_statement
+    {|class MyClass:
+    def method1(x):
+        return 3
+    def method2():
+        return 2 |};
+  [%expect
+    {|
+    (Class ((Identifier "MyClass"),
+       [(Function ((Identifier "method1"), [(Identifier "x")],
+           [(Return (Const (Int 3)))]));
+         (Function ((Identifier "method2"), [], [(Return (Const (Int 2)))]))]
+       )) |}]
+;;
+
+let%expect_test _ =
+  parser_tester pyParser show_statement "someClass.someField(10, 20 + 10)";
+  [%expect
+    {|
+    (Expression
+       (MethodCall ((Identifier "someClass"), (Identifier "someField"),
+          [(Const (Int 10)); (ArithOp (Add, (Const (Int 20)), (Const (Int 10))))]
+          ))) |}]
+;;
+
+let%expect_test _ =
+  parser_tester pyParser show_statement "someClass.someField = someValue";
+  [%expect
+    {|
+    (Assign ((Field ((Identifier "someClass"), (Identifier "someField"))),
+       (Variable (Global, (Identifier "someValue"))))) |}]
+;;
+
+let%expect_test _ =
+  parser_tester pyParser show_statement "lambda a,b: a*b";
+  [%expect
+    {|
+    (Expression
+       (Lambda ([(Identifier "a"); (Identifier "b")],
+          (ArithOp (Mul, (Variable (Global, (Identifier "a"))),
+             (Variable (Global, (Identifier "b")))))
+          ))) |}]
+;;
+
+let%expect_test _ =
+  parser_tester pyParser show_statement "f\"string{var}str\"";
+  [%expect
+    {|
+    (Expression
+       (FString
+          [(Str (String "string")); (Var (Identifier "var"));
+            (Str (String "str"))])) |}]
 ;;
 
 let%test _ = true = is_banned "return"
 let%test _ = true = is_digit '1'
 let%test _ = true = is_sign '-'
-let%test _ = parse p_integer "999" = Ok (Const (Int 999))
-let%test _ = parse p_string "\"Hello\"" = Ok (Const (String "Hello"))
-let%test _ = parse p_global_variable "a" = Ok (Variable (Global, Identifier "a"))
-
-let%test _ =
-  parse pyParser "for i in range(0, 3):\n    print(i)"
-  = Ok
-      [ For
-          ( Variable (Global, Identifier "i")
-          , [ Const (Int 0); Const (Int 3) ]
-          , [ Expression
-                (FunctionCall (Identifier "print", [ Variable (Global, Identifier "i") ]))
-            ] )
-      ]
-;;
-
-let%test _ =
-  parse pyParser "while 1 == 1:\n    print(1)"
-  = Ok
-      [ While
-          ( BoolOp (Equal, Const (Int 1), Const (Int 1))
-          , [ Expression (FunctionCall (Identifier "print", [ Const (Int 1) ])) ] )
-      ]
-;;
-
-let%test _ =
-  parse
-    pyParser
-    "class MyClass:\n\tdef method1(x):\n\t\treturn 3\n\tdef method2():\n\t\treturn 2"
-  = Ok
-      [ Class
-          ( Identifier "MyClass"
-          , [ Function
-                (Identifier "method1", [ Identifier "x" ], [ Return (Const (Int 3)) ])
-            ; Function (Identifier "method2", [], [ Return (Const (Int 2)) ])
-            ] )
-      ]
-;;
-
-let%test _ =
-  parse pyParser "someClass.someField(10, 20+10)"
-  = Ok
-      [ Expression
-          (MethodCall
-             ( Identifier "someClass"
-             , Identifier "someField"
-             , [ Const (Int 10); ArithOp (Add, Const (Int 20), Const (Int 10)) ] ))
-      ]
-;;
-
-let%test _ =
-  parse pyParser "someClass.someField = someValue"
-  = Ok
-      [ Assign
-          ( Field (Identifier "someClass", Identifier "someField")
-          , Variable (Global, Identifier "someValue") )
-      ]
-;;
-
-let%test _ = parse p_identifiers "x,y" = Ok [ Identifier "x"; Identifier "y" ]
-
-let%test _ =
-  parse pyParser "lambda a,b: a*b"
-  = Ok
-      [ Expression
-          (Lambda
-             ( [ Identifier "a"; Identifier "b" ]
-             , ArithOp
-                 ( Mul
-                 , Variable (Global, Identifier "a")
-                 , Variable (Global, Identifier "b") ) ))
-      ]
-;;
-
-let%test _ =
-  parse p_interpolationStrElemEndingWithCurly "string{" = Ok (Str (String "string"))
-;;
-
-let%test _ = parse p_interpolationVarElem "var}" = Ok (Var (Identifier "var"))
-let%test _ = parse p_interpolationStrElemLast "str\"" = Ok (Str (String "str"))
-
-let%test _ =
-  parse p_fString "f\"string{var}str\""
-  = Ok (FString [ Str (String "string"); Var (Identifier "var"); Str (String "str") ])
-;;
