3a5f25d6cb86a783c4baefda01946cef05dc4907
pasha <pasha@kub.myguest.virtualbox.org>
2024-01-09 01:43:54 +0300

    redo the interpreter.mli

--- Python/lib/interpreter.mli
+++ Python/lib/interpreter.mli
@@ -31,26 +31,55 @@ end
 module Eval : functor (M : MONADERROR) -> sig
   val fold_left : ('a -> 'b -> 'a M.t) -> 'a -> 'b list -> 'a M.t
   val map1 : ('a -> 'b M.t) -> 'a list -> 'b list M.t
-  val local_env : 'a
-  val temp_class_env : 'a
-  val global_env : 'a
-  val var_in_env : 'a -> 'b -> bool
-  val change_var : 'a -> 'b -> 'c -> 'd
-  val change_or_add_var : 'a -> 'b -> 'c
-  val change_or_add_var_list : '_weak3 -> '_weak4 list -> '_weak3
-  val get_var : 'a -> 'b -> 'c
-  val func_in_env : 'a -> 'b -> bool
-  val class_in_env : 'a -> 'b -> bool
-  val change_func : 'a -> 'b -> 'c
-  val change_or_add_func : 'a -> 'b -> 'c
-  val change_class : 'a -> 'b -> 'c
-  val change_or_add_class : 'a -> 'b -> 'c
-  val get_func : 'a -> 'b -> 'c
-  val get_class : 'a -> 'b -> 'c
-  val combine_args_and_params : 'a list -> 'b list -> 'c list
-  val get_str_from_identifier : 'a -> 'b
-  val pack_to_string : 'a -> 'b
-  val i_exp_or_stmt : 'a
-  val get_env : 'a -> 'b list -> 'a M.t
-  val interpret : '_weak1 list -> '_weak2 M.t
+
+  type var_symb =
+    { identifier : Ast.identifier
+    ; value : Ast.value
+    }
+
+  type function_symb =
+    { identifier : Ast.identifier
+    ; params : Ast.identifier list
+    ; body : Ast.statement list
+    }
+
+  type environment =
+    { id : Ast.identifier
+    ; local_envs : environment list
+    ; classes : environment list
+    ; functions : function_symb list
+    ; flag : Ast.flag
+    ; return_v : Ast.value
+    ; vars : var_symb list
+    ; vals_to_print : Ast.value list
+    }
+
+  val local_env : environment
+  val temp_class_env : environment
+  val global_env : environment
+  val var_in_env : Ast.identifier -> environment -> bool
+  val change_var : Ast.identifier -> Ast.value -> environment -> environment
+  val change_or_add_var : environment -> var_symb -> environment
+  val change_or_add_var_list : environment -> var_symb list -> environment
+  val get_var : Ast.identifier -> environment -> var_symb
+  val func_in_env : Ast.identifier -> environment -> bool
+  val class_in_env : Ast.identifier -> environment -> bool
+  val change_func : function_symb -> environment -> environment
+  val change_or_add_func : function_symb -> environment -> environment
+  val change_class : environment -> environment -> environment
+  val change_or_add_class : environment -> environment -> environment
+  val get_func : Ast.identifier -> environment -> function_symb
+  val get_class : Ast.identifier -> environment -> environment
+  val combine_args_and_params : Ast.identifier list -> Ast.value list -> var_symb list
+  val get_str_from_identifier : Ast.identifier -> string
+  val pack_to_string : Ast.value -> string M.t
+
+  type dispatch =
+    { i_expr : dispatch -> environment -> Ast.expression -> Ast.value M.t
+    ; i_stmt : dispatch -> environment -> Ast.statement -> environment M.t
+    }
+
+  val i_exp_or_stmt : dispatch
+  val get_env : environment -> Ast.statement list -> environment M.t
+  val interpret : Ast.statement list -> environment M.t
 end
