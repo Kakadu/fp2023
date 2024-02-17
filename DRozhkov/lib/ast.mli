396da532743aa1ec6f7ddeb1e02dfe77d672fdd1
RozhkovAleksandr <rozhkov_a@outlook.com>
2024-02-18 02:36:36 +0300

    fix: fix linter bugs for ast.mli

--- DRozhkov/lib/ast.mli
+++ DRozhkov/lib/ast.mli
@@ -1,62 +1,59 @@
-(** Copyright 2021-2023, Kakadu, RozhkovAleksandr *)
-
-(** SPDX-License-Identifier: LGPL-3.0-or-later *)
-
 type id = string
 
 val pp_id : Format.formatter -> id -> unit
 val show_id : id -> string
 
 type const =
-  | Int of int (** Integer *)
-  | Bool of bool (** Boolean *)
-  | Empty (** () *)
+  | Int of int
+  | Bool of bool
+  | Empty
 
 val pp_const : Format.formatter -> const -> unit
 val show_const : const -> string
 
 type pattern =
-  | PAny (**  _ *)
-  | PConst of const (** const *)
-  | PVar of id (** string *)
+  | PAny
+  | PConst of const
+  | PVar of id
 
 val pp_pattern : Format.formatter -> pattern -> unit
 val show_pattern : pattern -> string
 
 type bin_op =
-  | Add (** [+] *)
-  | Sub (** [-] *)
-  | Mul (** [*] *)
-  | Div (** [/] *)
-  | Eq (** [=] *)
-  | Neq (** [!=] *)
-  | Les (** [<] *)
-  | Leq (** [<=] *)
-  | Gre (** [>] *)
-  | Geq (** [>=] *)
+  | Add
+  | Sub
+  | Mul
+  | Div
+  | Eq
+  | Neq
+  | Les
+  | Leq
+  | Gre
+  | Geq
 
 val pp_bin_op : Format.formatter -> bin_op -> unit
 val show_bin_op : bin_op -> string
 
-type expression =
-  | Var of id (**string *)
-  | Const of const (** const *)
-  | IfThenElse of expression * expression * expression (** if ... then ... else ... *)
-  | Binop of bin_op * expression * expression (** 1 + 2 *)
-  | Fun of id * expression (** fun *)
-  | List of expression list (** [1;2;3] *)
-  | Match of expression * (pattern * expression) list (** match *)
-  | App of expression * expression (** f x *)
-  | Let of fun_rec * id * expression * expression option (** let *)
+type fun_rec =
+  | Rec
+  | NoRec
 
-and fun_rec =
-  | Rec (** let rec f*)
-  | NoRec (** let f *)
+val pp_fun_rec : Format.formatter -> fun_rec -> unit
+val show_fun_rec : fun_rec -> string
+
+type expression =
+  | Var of id
+  | Const of const
+  | IfThenElse of expression * expression * expression
+  | Binop of bin_op * expression * expression
+  | Fun of id * expression
+  | List of expression list
+  | Match of expression * (pattern * expression) list
+  | App of expression * expression
+  | Let of fun_rec * id * expression * expression option
 
 val pp_expression : Format.formatter -> expression -> unit
-val pp_fun_rec : Format.formatter -> fun_rec -> unit
 val show_expression : expression -> string
-val show_fun_rec : fun_rec -> string
 
 type expr_list = expression list
 
