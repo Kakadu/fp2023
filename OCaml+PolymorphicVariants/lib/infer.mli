4b2b6468675b578680ac255616a912f825d0ef8d
Nikita Nemakin <nikita.nemakin@yandex.ru>
2024-02-22 01:03:16 +0300

    Remove unnecessary declarations from .mli

--- OCaml+PolymorphicVariants/lib/infer.mli
+++ OCaml+PolymorphicVariants/lib/infer.mli
@@ -1,7 +1,3 @@
-(** Copyright 2023-2024, Nikita Nemakin *)
-
-(** SPDX-License-Identifier: LGPL-3.0-or-later *)
-
 type error =
   [ `No_variable of string
   | `Occurs_check
@@ -12,8 +8,14 @@ type error =
 
 val pp_error : Format.formatter -> error -> unit
 
+module VarSet : sig
+  type t = Set.Make(Base.Int).t
+end
+
+type scheme = S of VarSet.t * Typedtree.ty
+
 val run_infer
   :  Ast.structure
-  -> ((string, Typedtree.scheme, Base.String.comparator_witness) Base.Map.t, error) result
+  -> ((string, scheme, Base.String.comparator_witness) Base.Map.t, error) result
 
 val test_infer : string -> unit
