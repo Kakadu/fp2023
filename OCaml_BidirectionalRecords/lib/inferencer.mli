fb4eaddeac5765d0331a08375285b512c666f27c
ksenmel <ksemelashenko@gmail.com>
2024-03-04 03:53:43 +0300

    formatting

--- OCaml_BidirectionalRecords/lib/inferencer.mli
+++ OCaml_BidirectionalRecords/lib/inferencer.mli
@@ -4,11 +4,10 @@
 
 val run_inference : Ast.expr -> (Typing.typ, Typing.error) result
 
-module PP :
-  sig
-    val pp_type : Format.formatter -> Typing.typ -> unit
-    val pp_error : Format.formatter -> Typing.error -> unit
-    val print_typ : Typing.typ -> unit
-    val print_type_error : Typing.error -> unit
-    val print_result : Ast.expr -> unit
-  end
+module PP : sig
+  val pp_type : Format.formatter -> Typing.typ -> unit
+  val pp_error : Format.formatter -> Typing.error -> unit
+  val print_typ : Typing.typ -> unit
+  val print_type_error : Typing.error -> unit
+  val print_result : Ast.expr -> unit
+end
--- OCaml_BidirectionalRecords/lib/interpreter.mli
+++ OCaml_BidirectionalRecords/lib/interpreter.mli
@@ -3,14 +3,14 @@
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
 type error =
-    DivisionByZero
+  | DivisionByZero
   | UnboundValue of string
   | TypeMismatch
   | NotImplemented
   | PatternMatchingFailed
 
 type value =
-    VInt of int
+  | VInt of int
   | VBool of bool
   | VChar of char
   | VString of string
@@ -22,17 +22,17 @@ type value =
 
 and binding = (string, value, Base.String.comparator_witness) Base.Map.t
 
-module R :
-  sig
-    type ('ok, 'err) t = ('ok, 'err) result = Ok of 'ok | Error of 'err
-  end
+module R : sig
+  type ('ok, 'err) t = ('ok, 'err) result =
+    | Ok of 'ok
+    | Error of 'err
+end
 
 val run_expr_interpreter : Ast.expr -> (value, error) R.t
 
-module PP :
-  sig
-    val pp_value : Format.formatter -> value -> unit
-    val pp_error : Format.formatter -> error -> unit
-    val print_value : value -> unit
-    val print_error : error -> unit
-  end
+module PP : sig
+  val pp_value : Format.formatter -> value -> unit
+  val pp_error : Format.formatter -> error -> unit
+  val print_value : value -> unit
+  val print_error : error -> unit
+end
