8de7073dd9a08cb7733cb0da894d72aa1c8dc5a8
Kakadu <Kakadu@pm.me>
2024-07-24 18:50:54 +0300

    Fix Lambda template about unused declarations
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- Lambda/lib/tests.ml
+++ Lambda/lib/tests.ml
@@ -8,9 +8,9 @@ let rec fact n = if n = 1 then 1 else n * fact (n - 1)
 let%test _ = fact 5 = 120
 
 (* These is a simple unit test that tests a single function 'fact'
-  If you want to test something large, like interpretation of a piece
-  of a minilanguge, it is not longer a unit tests but an integration test.
-  Read about dune's cram tests and put the test into `demos/somefile.t`.
+   If you want to test something large, like interpretation of a piece
+   of a minilanguge, it is not longer a unit tests but an integration test.
+   Read about dune's cram tests and put the test into `demos/somefile.t`.
 *)
 
 open Lambda_lib
@@ -38,3 +38,15 @@ let%expect_test _ =
   Format.printf "%a" pp (parse_optimistically "(λf.λx. f (x x))");
   [%expect {| (Abs (f, (Abs (x, (App ((Var f), (App ((Var x), (Var x))))))))) |}]
 ;;
+
+let _ = Lambda_lib.Interpret.parse_and_run
+let _ = Lambda_lib.Lambda.a
+let _ = Lambda_lib.Lambda.one
+let _ = Lambda_lib.Lambda.p
+let _ = Lambda_lib.Lambda.three
+let _ = Lambda_lib.Lambda.two
+let _ = Lambda_lib.Lambda.without_strat
+let _ = Lambda_lib.Lambda.zero
+let _ = Lambda_lib.Parser.parse_lam
+let _ = Lambda_lib.Printast.pp
+let _ = Lambda_lib.Printast.show
